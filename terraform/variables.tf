c29a4a97cc7016681eedb9a5524df92a6fe889de
Ntnick-22 <nyeinthunaing322@gmail.com>
2025-09-05 00:33:38 +0100

    version updated

--- terraform/variables.tf
+++ terraform/variables.tf
@@ -7,7 +7,7 @@ variable "aws_region" {
 variable "project_name" {
   description = "Name of the project"
   type        = string
-  default     = "portfolio-dashboard-v2"
+  default     = "portfolio-dashboard-v3"
 }
 
 variable "vpc_cidr" {
