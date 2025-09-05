9255ccbe33a04cd2c584129ec6e302565ef6ab5c
Ntnick-22 <nyeinthunaing322@gmail.com>
2025-09-05 01:13:40 +0100

    fixed and deleted

--- terraform/variables.tf
+++ terraform/variables.tf
@@ -7,7 +7,7 @@ variable "aws_region" {
 variable "project_name" {
   description = "Name of the project"
   type        = string
-  default     = "portfolio-dashboard-v3"
+  default     = "portfolio-dashboard-v4"
 }
 
 variable "vpc_cidr" {
